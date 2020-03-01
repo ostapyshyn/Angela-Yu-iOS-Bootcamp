882e79be115f03363b578610d900ef1f6401777a
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-01 15:26:00 +0200

    add new projects

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Volodymyr Ostapyshyn on 22.02.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
