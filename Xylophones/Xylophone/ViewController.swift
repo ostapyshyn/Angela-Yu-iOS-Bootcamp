6c1aa64d81dc3dcece85a2b8b2259e459e367278
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-06 23:48:53 +0200

    add new feature

---
+++ Xylophones/Xylophone/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+        print(sender.currentTitle!)
+        
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
