141e121708a4108554f7f1f9fdbd8ee7c9b29f41
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-08 23:42:35 +0200

    add new project

--- Xylophones/Xylophone/ViewController.swift
+++ Xylophones/Xylophone/ViewController.swift
@@ -18,13 +18,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
         print(sender.currentTitle!)
+        print("Start")
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+            print("End")
+        }
         
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
