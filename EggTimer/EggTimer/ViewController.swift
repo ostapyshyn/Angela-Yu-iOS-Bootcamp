9496da207a998d051d5bbd43a36dd34b2c4b70de
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-11 23:43:15 +0200

    add new feature

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -7,34 +7,51 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var labelTop: UILabel!
     
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 8]
+    
+    var timeLeft = 60
+    
+    var timer = Timer()
     
     
-    @IBAction func hardnessSelected(_ sender: UIButton) {
+    @IBAction func hardnessSelected(_ sender: UIButton)  {
+        timer.invalidate()
         let hardness = sender.currentTitle!
         
         let time = eggTimes[hardness]!
         
-        let seconds = time * 60
         
-        for sec in (0...seconds).reversed() {
+        
             
-            
-                DispatchQueue.main.asyncAfter(deadline: .now() + 11) {
-                    print("\(sec) seconds")
-                    
-                }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timeLeft = time
+        
+        
 
-        }
-        
-        
     }
     
+    @objc func updateCounter() {
+        //example functionality
+        if timeLeft > 0 {
+            print("\(timeLeft) seconds.")
+            timeLeft -= 1
+        } else {
+            timer.invalidate()
+            labelTop.text = "DONE!"
+        }
+        
+    }
 }
