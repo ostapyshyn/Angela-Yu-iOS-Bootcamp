5cf8bd9fd69127daab6c74a4ceb0bb86a913a24b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-24 23:43:14 +0200

    add MVC

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,26 +14,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    
+    var stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
         
         
-
+        
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
-        print("Testing...")
+        switch sender.currentTitle {
+        case "Take a left.":
+            updateUI(1)
+        case "Take a right.":
+        updateUI(2)
+        default:
+            updateUI(0)
+        }
     }
     
-
-
+    func updateUI(_ num: Int) {
+        storyLabel.text = stories[num].title
+        choice1Button.setTitle(stories[num].choice1, for: .normal)
+        choice2Button.setTitle(stories[num].choice2, for: .normal)
+    }
+    
+    
+    
 }
 
--- Destini/Destini-iOS13/Model/Story.swift
+++ Destini/Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Story {
+    
+    var title: String
+    var choice1: String
+    var choice2: String
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
