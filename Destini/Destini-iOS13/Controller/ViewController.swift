dd23bf1ff0122438f62adeb7540cb12840008aa9
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-26 23:35:07 +0200

    add new structure

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -15,10 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     
     
-    var stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
+    
     
     
     
--- Destini/Destini-iOS13/Model/StoryBrain.swift
+++ Destini/Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,16 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    // Create a method (a function associated with an object) in the StoryBrain that’s called nextStory(), it should take a single input called userChoice which is of type String.
+    var storyNumber: Int
+    
+    
+    
+    
+    
+    var stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+}
