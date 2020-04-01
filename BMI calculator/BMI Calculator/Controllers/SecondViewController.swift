fb6bd0ac097f1063539e618913a0091d1babf18b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-01 22:35:07 +0300

    add new screen

---
+++ BMI calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Volodymyr Ostapyshyn on 01.04.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
