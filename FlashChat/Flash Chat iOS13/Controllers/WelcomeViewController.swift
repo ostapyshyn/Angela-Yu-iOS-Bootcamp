1a470d60ab7c800b64206acfae42194d2ad03e4b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-14 22:24:05 +0300

    Add new timer to print label

--- FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,16 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var charIndex = 0.0
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            
+            charIndex += 1
+        }
        
     }
-    
-
 }
