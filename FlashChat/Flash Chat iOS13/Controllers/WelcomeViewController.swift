7feb6a554c18d081efc68f24751ee1717fa35e88
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-16 22:54:47 +0300

    Add Firebase

--- FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -5,25 +5,30 @@
 //  Created by Angela Yu on 21/10/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
-
+import CLTypingLabel
 import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var charIndex = 0.0
-        titleLabel.text = ""
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            
-            charIndex += 1
-        }
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+        
+        
+//        var charIndex = 0.0
+//        titleLabel.text = ""
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//
+//            charIndex += 1
+//        }
        
     }
 }
