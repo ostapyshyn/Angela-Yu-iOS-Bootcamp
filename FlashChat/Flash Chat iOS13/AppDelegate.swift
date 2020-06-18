0493e73ca7720768fca96deaa0b42c76e3d89cee
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-18 23:46:39 +0300

    Add  IQKeyboardManager

--- FlashChat/Flash Chat iOS13/AppDelegate.swift
+++ FlashChat/Flash Chat iOS13/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Firebase
-
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         let db = Firestore.firestore()
         print(db)
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         
         return true
     }
